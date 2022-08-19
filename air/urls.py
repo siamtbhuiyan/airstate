435f57d0e58214690cb4d0dec0ddd6a067412b56
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2022-08-19 21:39:17 +0600

    Compare Multiple Data Sources

--- air/urls.py
+++ air/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register, name="register"),
     path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout")
+    path("logout", views.logout, name="logout"),
+    path("multiple-datasources", views.data_sources, name="data_sources")
 ]
--- air/views.py
+++ air/views.py
@@ -5,22 +5,35 @@ from django.urls import reverse
 
 from air.functions import get_aqi, login_required
 
+import pandas as pd
+from plotly.offline import plot
+import plotly.express as px
+
 @login_required
 def index(request):
     username = ""
     if request.session.has_key('username'):
         username = request.session['username']
     with connection.cursor() as cursor:
-        cursor.execute("SELECT date(time), pm25 FROM tblAirQuality WHERE division='Dhaka' ORDER BY time DESC")
+        cursor.execute("SELECT DATE(time), pm25 FROM tblAirQuality WHERE division='Dhaka' ORDER BY time DESC")
         current = cursor.fetchone()
         cursor.execute("SELECT DISTINCT division FROM tblAirQuality")
         divisionData = cursor.fetchall()
+        cursor.execute("SELECT DISTINCT strftime('%Y', time) FROM tblAirQuality")
+        yearData = cursor.fetchall()
+        cursor.execute("SELECT DISTINCT season FROM tblAirQuality")
+        seasonData = cursor.fetchall()
     currentDivision = "Dhaka"
     aqi = get_aqi(current[1])
     divisions = []
+    years = []
+    seasons = []
     for d in divisionData:
         divisions.append(d[0])
-    print(get_aqi(220.1))
+    for s in seasonData:
+        seasons.append(s[0])
+    for y in yearData:
+        years.append(y[0])
     if request.method == "POST":
         currentDivision = request.POST["division"]
         with connection.cursor() as cursor:
@@ -31,6 +44,8 @@ def index(request):
         "username": username,
         "currentDate": current[0],
         "divisions": divisions,
+        "seasons": seasons,
+        "years": years,
         "currentDivision": currentDivision,
         "aqi_value": aqi[0],
         "aqi_status": aqi[1],
@@ -90,3 +105,29 @@ def login(request):
 def logout(request):
     request.session.flush()
     return HttpResponseRedirect(reverse("index")) 
+
+@login_required
+def data_sources(request):
+    username = ""
+    if request.session.has_key('username'):
+        username = request.session['username']
+    if request.method == "POST":
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT pm25, DATE(time), organization FROM tblAirQuality WHERE season=%s AND DATE(time) LIKE %s", [request.POST["season"], str(request.POST["year"] + "%")])
+            data = cursor.fetchall()
+
+    cleaned_data = [
+        {
+            "Daily": d[1],
+            "Daily Avg PM2.5": d[0],
+            "Organization": d[2] 
+        } for d in data
+    ]
+
+    figure = px.line(cleaned_data, x="Daily", y="Daily Avg PM2.5", title=f"{request.POST['season']} {request.POST['year']}", color="Organization")
+    line_chart = plot(figure, output_type="div")
+
+    return render(request, "air/data-sources.html", {
+        "username": username,
+        "line_chart": line_chart
+    })
